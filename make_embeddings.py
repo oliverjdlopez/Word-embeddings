4a62e729da4432646d220b44d3d64714e9dfed77
ojl23 <ojl23@cornell.edu>
2023-12-03 12:08:03 -0500

    final b4 docing

--- make_embeddings.py
+++ make_embeddings.py
@@ -4,38 +4,45 @@ import torch
 import transformers
 import scipy as sp
 import numpy as np
+import enchant
 import time
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+
 
 def save_json(d, out_path):
     with open(out_path, "wt") as f:
         json.dump(d, f)
 
-def tokenize_and_embed(words):
+
+def tokenize_and_embed(words, layer):
     global tokenizer_time
     global inference_time
+    global use_pooler
     global device
-    
-    start = time.time()
-    tokens = tokenizer(words, return_tensors="pt", padding = True).to(device)
-    end = time.time()
-    tokenizer_time += end-start
 
     start = time.time()
-    outs= model(**tokens, output_hidden_states = True) #all hidden layers and embedding layer
-    #seqs = outs["last_hidden_state"] #last layer output
-    #seqs = outs["pooler_output"] #pooler output
-    #seqs = outs["hidden_states"] #pooler output
-    print(torch.equal(outs["hidden_states"][0], outs["last_hidden_states"]))
+    tokens = tokenizer(words, return_tensors="pt", padding=True).to(device)
+    end = time.time()
+    tokenizer_time += end - start
+
+    start = time.time()
+    # shape is [batch size, sequence length, model dim]
+    outs = model(**tokens,
+                 output_hidden_states=True)  # all hidden layers and initial embedding layer. outs["hidden_states"][-1] == outs["last_hidden_state"]
+    if use_pooler:
+        embeddings = outs["pooler_output"]
+        out = embeddings.cpu().numpy()  # pooler is always correct dim, don't need to do anything else
+    else:
+        embeddings = outs["hidden_states"][layer][:, 1:-1]  # choose hidden state, and then drop start and end tokens
+        out = (torch.mean(embeddings, 1)).squeeze().cpu().numpy()
     end = time.time()
     inference_time += end - start
-    # shape is [batch size, sequence length, model dim]
-    embeddings = seqs[: , 1:-1] #drop start and end tokens
-    return (torch.mean(embeddings, 1)).squeeze().cpu().numpy()
+    return out
 
 
-def make_vocab(out_path, max_vocab_size, only_alpha = True):
+def make_vocab(out_path, max_vocab_size, only_alpha=True):
+    d = enchant.Dict("en_US")
     vocab = {}
     n_words = 0
     with open("vocab.txt", "rt", errors="replace") as f:
@@ -46,7 +53,7 @@ def make_vocab(out_path, max_vocab_size, only_alpha = True):
                 split = line.split()
                 word, count = split[0], split[1]
                 if only_alpha:
-                    if word.isalpha():
+                    if word.isalpha() and d.check(word):
                         vocab[word] = count
                         n_words += 1
                 else:
@@ -54,8 +61,9 @@ def make_vocab(out_path, max_vocab_size, only_alpha = True):
                     n_words += 1
     return vocab
 
-#take vocab dict and return dict with dim reduced embeddings
-def make_embeddings(vocab, embedder, batch_size = 1):
+
+# take vocab dict and return dict with dim reduced embeddings
+def make_embeddings(vocab, embedder, layer, batch_size=1):
     global sklearn_time
     n_words = len(vocab)
     embeddings = np.zeros((n_words, embedding_dim))
@@ -63,14 +71,13 @@ def make_embeddings(vocab, embedder, batch_size = 1):
     for i, k in enumerate(vocab.keys()):
         words[i % batch_size] = k
         nth_word = i + 1
-        if (nth_word) % batch_size == 0: #for now just assume batch_size divides vocab
-            e = embedder(words) #e is numpy array with embeddings for words
+        if (nth_word) % batch_size == 0:  # for now just assume batch_size divides vocab
+            e = embedder(words, layer)  # e is numpy array with embeddings for words
             embeddings[nth_word - batch_size: nth_word] = e
-
 
     start = time.time()
     pca_embeddings = PCA(n_components=pca_components).fit_transform(embeddings)
-    tsne_embeddings = TSNE(n_components=tsne_components).fit_transform(pca_embeddings)
+    tsne_embeddings = TSNE(n_components=tsne_components).fit_transform(embeddings)
     end = time.time()
     sklearn_time += end - start
 
@@ -78,10 +85,9 @@ def make_embeddings(vocab, embedder, batch_size = 1):
     for i, w in enumerate(vocab.keys()):
         x = tsne_embeddings[i][0]
         y = tsne_embeddings[i][1]
-        d[w] = {"idx": int(i%142), "x": float(x), "y": float(y)} #142 is because there are 142 colors in the json file, will have to address later
+        d[w] = {"idx": int(i % 142), "x": float(x),
+                "y": float(y)}  # 142 is because there are 142 colors in the json file, will have to address later
     return d
-
-
 
 
 tokenizer_time = 0
@@ -89,62 +95,39 @@ inference_time = 0
 sklearn_time = 0
 
 if __name__ == "__main__":
+    # model = transformers.BertModel.from_pretrained("bert-base-uncased")
+    # model.save_pretrained("bert_model")
+    # tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-uncased")
+    # tokenizer.save_pretrained("bert_tokenizer")
+    # max_vocab_size = 96000
+    # vocab_path = "vocab_eng_" + str(int(max_vocab_size)) + ".json"
+    # vocab_json = make_vocab(vocab_path, max_vocab_size)
+    # save_json(vocab_json, vocab_path)
+
     batch_size = 32
-    max_vocab_size = batch_size  * 3000
+    max_vocab_size = batch_size * 3000
     pca_components = 50
     tsne_components = 2
     embedding_dim = 768
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
-    vocab_path = "vocab_" + str(int(max_vocab_size)) + ".json"
-    embedding_path = "embeddings_" + str(int(max_vocab_size)) + ".json"
+    use_pooler = False
     model_path = "bert_model"
     tokenizer_path = "bert_tokenizer"
-
-    with torch.no_grad():
-        # model = transformers.BertModel.from_pretrained("bert-base-uncased")
-        # model.save_pretrained("bert_model")
-        # tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-uncased")
-        # tokenizer.save_pretrained("bert_tokenizer")
-        vocab_json = make_vocab(vocab_path, max_vocab_size)
-        save_json(vocab_json, vocab_path)
-
-
-        model = transformers.AutoModel.from_pretrained(model_path, local_files_only=True).to(device)
-        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
-        
-        with open(vocab_path, "rt") as f:
-            vocab = json.load(f)
-            start = time.time()
-            embeddings = make_embeddings(vocab, tokenize_and_embed, batch_size=batch_size)
-            end = time.time()
-            save_json(embeddings, embedding_path)
-            print("Total embedding time was " + str(end-start))
-            print("sklearn time was" + str(sklearn_time))
-            print("Inference time was" + str(inference_time))
-            print("tokenization time was" + str(tokenizer_time))
-   
-   
-   
-   
-   
-   
-   
-    # #testing output dims
-    # multi_count = 0
-    # errs = 0
-    # with open("vocab.txt", "r") as f:
-    #     i = 0
-    #     while i < vocab_size:
-    #         try:
-    #             line = f.readline()
-    #             word = line.split()[0]      
-    #             embedding = tokenize_and_embed(model, tokenizer, word)
-    #             if embedding.shape != (1,3,768):
-    #                 multi_count += 1
-    #         except:
-    #             errs += 1
-    #         i += 1
-
-    # print("the number of non-singular words is " + str(multi_count))
-    # print("the number of errors thrown was " + str(errs))
+    vocab_path = "vocab_eng_" + str(int(max_vocab_size)) + ".json"
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    layers_of_interest = [0, -1, -2, -10]
+    for layer in layers_of_interest:
+            suffix = "pooler" if use_pooler else str(layer)
+            embedding_path = "embeddings_" + str(int(max_vocab_size)) + "_" + suffix + ".json"
+            with torch.no_grad():
+                model = transformers.AutoModel.from_pretrained(model_path, local_files_only=True).to(device)
+                tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
+                with open(vocab_path, "rt") as f:
+                    vocab = json.load(f)
+                    start = time.time()
+                    embeddings = make_embeddings(vocab, tokenize_and_embed, layer, batch_size=batch_size)
+                    end = time.time()
+                    save_json(embeddings, embedding_path)
+                    print("Total embedding time was " + str(end - start))
+                    print("sklearn time was" + str(sklearn_time))
+                    print("Inference time was" + str(inference_time))
+                    print("tokenization time was" + str(tokenizer_time))
--- map-maker.py
+++ map-maker.py
@@ -11,77 +11,79 @@ from subprocess import check_call
 from random import sample
 import json
 
-path_suffix = "_96000_10"
-data = json.load(open("./embeddings" + path_suffix + ".json"))
-colors = json.load(open("./primarycolors.json"))
-vs = data.values()
-ks = data.keys()
-x = np.array([-t['x'] for t in vs])
-y = np.array([-t['y'] for t in vs])
-c = np.array([colors[t['idx']] for t in vs])
-w = np.array([t for t in ks])
-frac = np.random.rand(len(w))
-frac[(-len(w) // 2):] = 1
+suffixes = ["_96000_" + str(n) for n in [-1, -2, -10, 0]]
+for path_suffix in ["_96000_-2"]:
+
+    data = json.load(open("./embeddings" + path_suffix + ".json"))
+    colors = json.load(open("./primarycolors.json"))
+    vs = data.values()
+    ks = data.keys()
+    x = np.array([-t['x'] for t in vs])
+    y = np.array([-t['y'] for t in vs])
+    c = np.array([colors[t['idx']] for t in vs])
+    w = np.array([t for t in ks])
+    frac = np.random.rand(len(w))
+    frac[(-len(w) // 2):] = 1
 
 
 
-pd = .0256
-xbds = np.array([x.min(), x.max()]) + np.array([-pd, pd])
-ybds = -np.array([y.max(), y.min()]) + np.array([-pd, pd])
+    pd = .0256
+    xbds = np.array([x.min(), x.max()]) + np.array([-pd, pd])
+    ybds = -np.array([y.max(), y.min()]) + np.array([-pd, pd])
 
 
 
-seed_words = {'the', 'bose_einstein_condensation', 'quantum_spin_hall_effect', 'parsed', 'qubit', 'spiking_neurons',
-              'quark', 'black_branes', 'vision_tasks', 'unfortunate', 'graphene', 'plotstyle', 'facile', 'genetic',
-              'groups', 'subschemes', 'finance', 'scholarly', 'logistic', 'smith', 'inflation', 'telescope',
-              'polymorphic', 'divisibility', 'holomorphic', 'paraboloid', 'hay', 'bremen', 'sunspot', 'liquid',
-              'nanorod', 'milky_way_galaxy', 'viscosity', 'maser', 'halo', 'metal_rich', 'supersymmetry', 'wi_fi',
-              'checksum', 'igor', 'cornell_university', 'portal', 'kmeans', 'multistep', 'optimization'}
+    seed_words = {'the', 'bose_einstein_condensation', 'quantum_spin_hall_effect', 'parsed', 'qubit', 'spiking_neurons',
+                  'quark', 'black_branes', 'vision_tasks', 'unfortunate', 'graphene', 'plotstyle', 'facile', 'genetic',
+                  'groups', 'subschemes', 'finance', 'scholarly', 'logistic', 'smith', 'inflation', 'telescope',
+                  'polymorphic', 'divisibility', 'holomorphic', 'paraboloid', 'hay', 'bremen', 'sunspot', 'liquid',
+                  'nanorod', 'milky_way_galaxy', 'viscosity', 'maser', 'halo', 'metal_rich', 'supersymmetry', 'wi_fi',
+                  'checksum', 'igor', 'cornell_university', 'portal', 'kmeans', 'multistep', 'optimization'}
 
-# no daniela
-dir_base = os.path.join(os.getcwd(), "zoom" + path_suffix)
-zooms = 8
-spacings = np.linspace(0, 1, zooms + 1) ** 6
-spacings[0] = -1
-spacings[1] = -1
-for zoom in range(zooms + 1):
-    print("ZOOM", zoom, spacings[zoom])
-    divs = 1 << zoom
-    xscale = np.diff(xbds) / divs
-    yscale = np.diff(ybds) / divs
-    dir_zoom = os.path.join(dir_base, str(zoom))
-    check_call(['mkdir', dir_zoom], shell = True)
-    for i in range(divs):
-        dir_slice = os.path.join(dir_zoom, str(i))
-        check_call(['mkdir', dir_slice], shell = True)
-        for j in range(divs):
-            # print(i,j)
-            x0 = xscale * i + xbds[0]
-            y0 = ybds[1] - (yscale * (j + 1))
-            mask = (x > x0 - xscale) & (x < x0 + 2 * xscale) & (-y > y0 - yscale) & (-y < y0 + 2 * yscale)
+    # no daniela
+    dir_base = os.path.join(os.getcwd(), "zoom" + path_suffix)
+    zooms = 8
+    spacings = np.linspace(0, 1, zooms + 1) ** 6
+    spacings[0] = -1
+    spacings[1] = -1
+    for zoom in range(zooms + 1):
+        print("ZOOM", zoom, spacings[zoom])
+        divs = 1 << zoom
+        xscale = np.diff(xbds) / divs
+        yscale = np.diff(ybds) / divs
+        dir_zoom = os.path.join(dir_base, str(zoom))
+        check_call(['mkdir', '-p', dir_zoom])
+        for i in range(divs):
+            dir_slice = os.path.join(dir_zoom, str(i))
+            check_call(['mkdir', '-p', dir_slice])
+            for j in range(divs):
+                # print(i,j)
+                x0 = xscale * i + xbds[0]
+                y0 = ybds[1] - (yscale * (j + 1))
+                mask = (x > x0 - xscale) & (x < x0 + 2 * xscale) & (-y > y0 - yscale) & (-y < y0 + 2 * yscale)
 
-            xs = x[mask]
-            ys = y[mask]
-            cs = c[mask]
-            ws = w[mask]
-            fracs = frac[mask]
+                xs = x[mask]
+                ys = y[mask]
+                cs = c[mask]
+                ws = w[mask]
+                fracs = frac[mask]
 
-            fig, axs = pl.subplots(figsize=(10, 10))
-            axs.scatter(xs, -ys, c=cs, edgecolors="#eeeeee", lw=0.0, s=5 * divs, alpha=0.5)
-            axs.set_xlim([x0, x0 + xscale])
-            axs.set_ylim([y0, y0 + yscale])
-            axs.axis('off')
-            axs.grid('off')
-            pl.subplots_adjust(0, 0, 1, 1, 0, 0)
+                fig, axs = pl.subplots(figsize=(10, 10))
+                axs.scatter(xs, -ys, c=cs, edgecolors="#eeeeee", lw=0.0, s=5 * divs, alpha=0.5)
+                axs.set_xlim([x0, x0 + xscale])
+                axs.set_ylim([y0, y0 + yscale])
+                axs.axis('off')
+                axs.grid('off')
+                pl.subplots_adjust(0, 0, 1, 1, 0, 0)
 
-            for w0, x0, y0, frac0 in zip(ws, xs, ys, fracs):
-                if zoom == zooms or (zoom > 2 and frac0 <= spacings[zoom]) \
-                        or (zoom >= 2 and w0 in seed_words):
-                    txt = axs.text(x0, -y0, w0, fontsize=40, alpha=0.72)
-                    txt.set_bbox(dict(color='white', alpha=0.72 if zoom > 2 else .25,  # was .8
-                                      edgecolor='white'))  # was .8
+                for w0, x0, y0, frac0 in zip(ws, xs, ys, fracs):
+                    if zoom == zooms or (zoom > 2 and frac0 <= spacings[zoom]) \
+                            or (zoom >= 2 and w0 in seed_words):
+                        txt = axs.text(x0, -y0, w0, fontsize=40, alpha=0.72)
+                        txt.set_bbox(dict(color='white', alpha=0.72 if zoom > 2 else .25,  # was .8
+                                          edgecolor='white'))  # was .8
 
-            pl.savefig(os.path.join(dir_slice, str(j) + ".png"), dpi=25.6)
-            pl.close('all')
+                pl.savefig(os.path.join(dir_slice, str(j) + ".png"), dpi=25.6)
+                pl.close('all')
 
-    if len(sys.argv) > 1 and sys.argv[1] == 's' and zoom == 2: break
+        if len(sys.argv) > 1 and sys.argv[1] == 's' and zoom == 2: break
